1d009ca02ada34355a403e1be78883437cd5f02e
Israel <israelromang@hotmail.com>
2022-05-06 17:59:43 -0500

    utils/input_out.py: Definind different reading functions

--- utils/input_out.py
+++ utils/input_out.py
@@ -1,18 +1,29 @@
 #! /usr/bin/env python
 import argparse as ap
-
+import os
+import pandas
 
 def main():
-    print("Espectral signal analysis")
+    print("Spectral signal analysis")
     args = arg_parser()
-    print(args)
+    text_files = read_folder_content(args.foldername)
+    
+    for text_file in text_files:
+        content = read_file_content(text_file)
+        print(content)
 
-def read_folder_files():
-    pass
+def read_file_content(filename: str) -> pandas.DataFrame:
+    content = pandas.read_csv(filename) 
+    return content
 
-def arg_parser():
+def read_folder_content(foldername: str) -> list:
+    files = os.listdir(foldername)
+    return files
+
+def arg_parser() -> ap.Namespace:
     parser = ap.ArgumentParser(description="Espectral signal analysis")
-    parser.add_argument("-f", "--folder", help="Folder name where the spectral files are", required=True)
+    parser.add_argument("-f", "--foldername", help="Folder name where the spectral files are", required=True)
+    parser.add_argument("-o", "--output", help="Output file name")
     return parser.parse_args()
 
 if __name__ == "__main__":

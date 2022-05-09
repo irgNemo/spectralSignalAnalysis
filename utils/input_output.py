e2ca16eab8570f214c0f88a2b2cca57a54bf0e2d
Israel <israelromang@hotmail.com>
2022-05-09 18:23:22 -0500

    File generator

--- utils/input_output.py
+++ utils/input_output.py
@@ -7,21 +7,36 @@ def main():
     print("Spectral signal analysis")
     args = arg_parser()
     text_files = read_folder_content(args.foldername)
-    dataframe_list = load_folder_csv(text_files, args.foldername)
-    concatenate_signal_spectrum(dataframe_list)
+    dataframe_dict = load_folder_csv(text_files, args.foldername)
+
+    for dataframe in dataframe_dict:
+        dataframe.
 
 
-def concatenate_signal_spectrum(dataframe_list:list):
-    for dataframe in dataframe_list:
-        print(dataframe.iloc[:, 0:])
+
+    #spectrums_dataframe = concatenate_signal_spectrum(dataframe_dict)
+    #spectrums_dataframe.to_csv(args.output,  header=True, index=False)
+
+
+def concatenate_signal_spectrum(dataframes: dict) -> pandas.DataFrame:
+    data = {}
+
+    for i, key in enumerate(dataframes.keys()):
+        dataframe = dataframes[key]
+        column_name = "{}".format(key)
+        if i == 0:
+            data['wave_length'] = dataframe.iloc[:, 0:1][0].values.tolist()
+        data[column_name] = dataframe.iloc[:, 1:2][1].values.tolist()
+
+    return pandas.DataFrame(data)
     
 
-def load_folder_csv(text_files: list, folder_path: str) -> list:
-    dataframe_list = []
+def load_folder_csv(text_files: list, folder_path: str, header=None) -> list:
+    dataframe_dict = {}
     for text_file in text_files:
         file_path = "{0}{1}".format(folder_path, text_file)
-        dataframe_list.append(pandas.read_csv(file_path))
-    return dataframe_list
+        dataframe_dict[text_file] = pandas.read_csv(file_path, header=header, delim_whitespace=True)
+    return dataframe_dict
 
 
 def read_folder_content(foldername: str) -> list:

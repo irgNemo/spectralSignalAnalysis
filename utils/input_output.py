30e3c201b4fc2a2003a100dd826e0b13c471154e
Israel Roman <israel.roman@cucei.udg.mx>
2022-05-08 23:18:14 -0500

    Adding functions

--- utils/input_output.py
+++ utils/input_output.py
@@ -7,25 +7,34 @@ def main():
     print("Spectral signal analysis")
     args = arg_parser()
     text_files = read_folder_content(args.foldername)
-    
-    for text_file in text_files:
-        file_path = "{0}{1}".format(args.foldername, text_file)
-        content = read_file_content(file_path)
-        print(content)
+    dataframe_list = load_folder_csv(text_files, args.foldername)
+    concatenate_signal_spectrum(dataframe_list)
 
-def read_file_content(filename: str) -> pandas.DataFrame:
-    content = pandas.read_csv(filename) 
-    return content
+
+def concatenate_signal_spectrum(dataframe_list:list):
+    for dataframe in dataframe_list:
+        print(dataframe.iloc[:, 0:])
+    
+
+def load_folder_csv(text_files: list, folder_path: str) -> list:
+    dataframe_list = []
+    for text_file in text_files:
+        file_path = "{0}{1}".format(folder_path, text_file)
+        dataframe_list.append(pandas.read_csv(file_path))
+    return dataframe_list
+
 
 def read_folder_content(foldername: str) -> list:
     files = os.listdir(foldername)
     return files
+
 
 def arg_parser() -> ap.Namespace:
     parser = ap.ArgumentParser(description="Espectral signal analysis")
     parser.add_argument("-f", "--foldername", help="Folder name where the spectral files are", required=True)
     parser.add_argument("-o", "--output", help="Output file name")
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
